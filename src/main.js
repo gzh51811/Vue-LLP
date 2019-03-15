50245a7c41fb001aaa89e3dde5c493c4ae5377ce
limi-son <1464398816@qq.com>
2019-03-15 19:46:32 +0800

    版本1

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from "./router";
+import flexible from 'lib-flexible'
+// import 'lib-flexible/flexible.js'
+
+Vue.use(flexible)
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
